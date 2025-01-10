5c5d2c932e6c995c04201d26beb293625abcacee
AayushKThakur <connecttoakt@gmail.com>
2025-01-10 17:54:06 +0530

    Week 2 JS

---
+++ Week_2/index.js
@@ -0,0 +1,24 @@
+function sum(a,b){
+    return a + b;
+}
+let ans = sum(20,30);
+// console.log(ans);
+
+function summ(n){
+    //return n * (n+1);
+    let ans = 0;
+    for (let i = 0; i <= n; i++) {
+        ans = ans +i;
+    }
+    return ans;
+}
+const answ = summ(10);
+const { log } = require("console");
+// console.log(answ);
+
+const fs = require("fs"); //importing an external library "fs"
+// console.log(fs);
+
+const data = fs.readFileSync('./a.txt', 'utf-8'); //calls fs.readfile synchronously
+console.log(data); //logs the data over here
+
