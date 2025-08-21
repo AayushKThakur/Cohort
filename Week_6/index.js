42a550378c0265e8fcd2b711da6b2d7dbac2b300
AayushKThakur <connecttoakt@gmail.com>
2025-08-21 19:04:46 +0530

    Week-6-token

---
+++ Week_6/index.js
@@ -0,0 +1,142 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+//shoudl return a random long string
+function generateToken() {
+  let options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let token = "";
+  for (let i = 0; i < 32; i++) {
+    // use a simple function here
+    token += options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
+
+app.post("/signup", function (req, res) {
+  //later: input validations using zod for username and pw.
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //check for : already signed in users
+  //   if(users.find(u => u.username ===username)){
+  //     res.json({
+  //         message: "You are Already signed up "
+  //     })
+  //     return
+  //   }
+
+  users.push({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: "You are signed up",
+  });
+  console.log(users);
+});
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  let foundUser = null; //instance of the user ; the in memory for the user
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username && users[i].password == password) {
+      foundUser = users[i];
+    }
+  }
+
+  if (foundUser) {
+    const token = generateToken();
+    foundUser.token = token;
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).send({
+      message: "Inavalid username or password",
+    });
+  }
+
+  console.log(users);
+
+  //   const user = user.find(function (u) {
+  //     if ((u, username == username && u.password == password)) {
+  //       return true;
+  //     } else {
+  //       false;
+  //     }
+  //   });
+});
+
+app.listen(3000);
+// the https server is listening on port 3000
