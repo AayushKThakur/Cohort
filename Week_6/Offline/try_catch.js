a62f1e78e5df210c838979bacd2ce96689c973c8
AayushKThakur <connecttoakt@gmail.com>
2025-09-26 13:42:47 +0530

    Week-6-offline

---
+++ Week_6/Offline/try_catch.js
@@ -0,0 +1,12 @@
+function getLength(name) {
+  return name.length;
+}
+
+try {
+  const ans = getLength();
+  console.log(ans);
+} catch (error) {
+  console.log("There is an error");
+}
+
+console.log("hi there from akt");
